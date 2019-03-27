f38b1f627384c8ded06603d6efc22f9124e23e70
kingmax <kingmax_res@163.com>
2019-03-27 18:44:39 +0800

    + using Pawn MovementComponent

---
+++ cppHowToComponents/cppHowToComponents/CollidingPawnMovementComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CollidingPawnMovementComponent.h"
+
+void UCollidingPawnMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!PawnOwner || !UpdatedComponent || ShouldSkipUpdate(DeltaTime))
+	{
+		return;
+	}
+
+	// 获取（然后清除）我们在ACollidingPawn::Tick中设置的移动矢量
+	FVector DesiredMovementThisFrame = ConsumeInputVector().GetClampedToMaxSize(1.0f) * DeltaTime * 150.0f;
+	if (!DesiredMovementThisFrame.IsNearlyZero())
+	{
+		FHitResult Hit;
+		SafeMoveUpdatedComponent(DesiredMovementThisFrame, UpdatedComponent->GetComponentRotation(), true, Hit);
+
+		// 如果撞到某个东西，则尝试沿着滑动
+		if (Hit.IsValidBlockingHit())
+		{
+			SlideAlongSurface(DesiredMovementThisFrame, 1.0f - Hit.Time, Hit.Normal, Hit);
+		}
+	}
+}
+
+
